35b44df12f801dff76bee6d8fcaff9f9f4836da4
Alex Rattray <rattray.alex@gmail.com>
2015-09-02 19:50:28 -0700

    hello world & basic styling

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import React from 'react'
 
-console.log('hello, world', React)
+import './base.css'
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <center>
+          <h1>
+            Store Picker
+          </h1>
+        </center>
+        <div className='site-wrapper'>
+          Hello, World!
+        </div>
+      </div>
+    )
+  }
+}
+
+
+React.render(
+  <App />,
+  document.body
+)
